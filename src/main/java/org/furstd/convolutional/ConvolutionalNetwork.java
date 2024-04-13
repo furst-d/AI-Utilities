65d06468eca2907297522dba64e922ceaf4173c2
Dominik Furst <dominikfurst1@seznam.cz>
2024-04-13 11:16:03 +0200

    Removed unused method

--- src/main/java/org/furstd/convolutional/ConvolutionalNetwork.java
+++ src/main/java/org/furstd/convolutional/ConvolutionalNetwork.java
@@ -71,18 +71,6 @@ public class ConvolutionalNetwork {
         }
     }
 
-    private double findMax(double[][] matrix) {
-        double max = matrix[0][0];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] > max) {
-                    max = matrix[i][j];
-                }
-            }
-        }
-        return max;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ConvolutionalNetwork cnn = new ConvolutionalNetwork();
